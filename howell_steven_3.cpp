f5be9a603e71fc341f18a60c2fc602ba2d05e6bc
sjhjrcode <Steven.J.Howell.Code@gmail.com>
2022-10-18 11:27:49 -0500

    fifth commit

--- howell_steven_3.cpp
+++ howell_steven_3.cpp
@@ -1,4 +1,4 @@
-
+//Steven Howell Homework 3
 
 #include <iostream>
 #include <stack>
@@ -9,9 +9,10 @@ using namespace std;
 
 
 
-int hor = 0;
+int hor = 0; //global dimensions for the width and height of the map
 int ver = 0;
 /*
+ * //test code that gives correct response for comparison
 void PrintOut(int *arr, int i, int j, int m, int n, int *path, int pi)
 {
 
@@ -53,7 +54,7 @@ void PrintOut(int *arr, int i, int j, int m, int n, int *path, int pi)
 
 
 
-
+//attempt 1 using 2d array failure does not work crashes program
 
 void PrintOut2(int *arr, vector<vector<int> > mapping, int i, int j, int m, int n, vector<int> path = {})
 {
@@ -88,27 +89,17 @@ void PrintOut2(int *arr, vector<vector<int> > mapping, int i, int j, int m, int 
 }
 
 */
+//attempt2 works using 2d array and stores data as a vector
+/*
+ * arr is 2d array pointer int i, j is the current position in the map m and n are the size of the map
+ * path is the current path that is blank upon first initialization and then gets updated each time
+ */
 void PrintOut3(int **arr, int i, int j, int m, int n, vector<int> path = {})
 {
 
-    /*
-    if (i < 0 || j < 0 || i >= m ||
-        j >= n || mapping[i][j] == 1)
-        return;
-    if(i == m-1 && j==n-1){
-        path.push_back(arr[i][j]);
-
-        for(int tmp = 0; tmp < path.size();tmp++){
-            cout<<path[tmp]<<"";
-        }
-        cout<<endl;
-        return;
-
-    }
-*/
 
     //cout<<"i and m "<<i<<" "<<m<<endl;
-    if (i == m - 1)
+    if (i == m - 1) //if i is at the last column then use for loop to push back all the possible paths
     {
         //cout<<"i if statement triggered"<<endl;
         for (int k = j; k < n; k++) {
@@ -116,38 +107,38 @@ void PrintOut3(int **arr, int i, int j, int m, int n, vector<int> path = {})
             path.push_back(arr[i][k]);
             //cout<<*((arr + i * n) + k)<<endl;
         }
-        for (int l = 0; l < path.size(); l++)
-            cout << path[l] << " ";
+        for (int l : path)
+            cout << l << " ";
         cout << endl;
-        return;
+        return;// program reaches it collusion
     }
     //cout<<"j and n "<<j<<" "<<n<<endl;
-    if (j == n - 1)
+    if (j == n - 1)//if j is at the last row then use for loop to push back all the possible paths
     {
         for (int k = i; k < m; k++) {
             //cout<<arr[k][j]<<endl;
             path.push_back(arr[k][j]);
         }
         for (int l : path)
-            cout << l << " ";
+            cout << l << " "; //print all values in path
         cout << endl;
-        return;
+        return; // program reaches it collusion
     }
 
 
     //mapping[i][j] = 1;
-    path.push_back(arr[i][j]);
+    path.push_back(arr[i][j]); // store all movements that are not the end in the array based on the current cell i,j
 
-    // Print all the paths that are possible after moving down
+    // Print out the paths that are possible after moving down
     PrintOut3(arr, i + 1, j, m, n, path);
 
-    // Print all the paths that are possible after moving right
+    // Print out paths that are possible after moving right
     PrintOut3(arr, i, j + 1, m, n, path);
 
-    // Print all the paths that are possible after moving diagonal
+    // Print out the paths that are possible after moving diagonal
     PrintOut3(arr, i + 1, j + 1, m, n, path);
 
-    path.pop_back();
+    path.pop_back(); // backtrack on failure by clearing the last position in the path
     //mapping[i][j] = 0;
 }
 
@@ -173,13 +164,23 @@ void Start_Print(int *mx, int m, int n)
     PrintOut2(mx,mapping, 0, 0, m, n);
 }
  */
+//this was mostly here for figuring out how to put a 2d array into printout properly and do all adjustments in an isolated method.
 void Start_Print2(int **mx, int m, int n)
 {
-    vector<int> path;
+    //vector<int> path;
     //vector<vector<int> > mapping(2, vector<int>(3, 0));
-    PrintOut3(mx, 0, 0, m, n);
-}
+    PrintOut3(mx, 0, 0, m, n); //start the print-out
 
+    // Delete the array created
+    for (int i = 0; i < m; i++) // To delete the inner
+        // arrays
+        delete[] mx[i];
+    delete[] mx; // To delete the outer array
+    // which contained the pointers
+    // of all the inner arrays
+
+}
+//print menu
 void menu(){
     std::cout.width(20);
     std::cout<<"MENU"<<std::endl<<std::endl<<"Horizontal Axis (0), Vertical Axis (1), Start \n"
@@ -192,7 +193,7 @@ void menu(){
 
 
 
-
+// make a disposition based on input
 std::string decision(const std::string& user_input){
     std::string key = "null";
 
@@ -252,28 +253,28 @@ while (!ss.eof()) {
 
 
     if(wordlist.at(0) == "0"){
-        hor = stoi(wordlist.at(1));
+        hor = stoi(wordlist.at(1)); //store the array horizontal dimensions
     }
 
     else if(wordlist.at(0) == "1"){
-        ver = stoi(wordlist.at(1));
+        ver = stoi(wordlist.at(1)); //store the array vertical dimensions
 
 
     }
 
     else if(wordlist.at(0) == "2"){
-        if(hor != 0 && ver!=0){
+        if(hor != 0 && ver!=0){//checks to make sure array has size
 
             int count = 0;
 
-            int **map = new int*[hor];
+            int **map = new int*[hor]; //make 2d array using dynamic arrays modified to not need memory address
             //int map2[hor][ver];
 
             for (int i = 0; i < hor; i++) {
 
                 // Declare a memory block
                 // of size n
-                map[i] = new int[ver];
+                map[i] = new int[ver];//add vertical to dynamic array
             }
             /*
 
@@ -290,10 +291,10 @@ while (!ss.eof()) {
              */
 
             //count = 0;
-            for (int j = 0; j < hor; j++) {
-                for (int i = 0; i < ver; i++) {
+            for (int i = 0; i < hor; i++) {
+                for (auto j = 0; j < ver; j++) {
 
-                    map[i][j] = ++count;
+                    map[i][j] = ++count;// assign values to each position in the array
                     //cout<<map[i][j]<<" ";
 
 
@@ -302,7 +303,7 @@ while (!ss.eof()) {
             }
             //PAP(*map2, hor, ver);
             //cout<<endl;
-            Start_Print2(map, hor, ver);
+            Start_Print2(map, hor, ver);//print out array
             /*
             int* map = new int[hor*ver];
 
@@ -349,10 +350,10 @@ while (!ss.eof()) {
 
         }
         else if(ver != 0){
-            cout<<"make sure the horizontal column has a value"<<endl;
+            cout<<"make sure the horizontal column has a value"<<endl; // error code
         }
         else{
-            cout<<"make sure the vertical column has a value"<<endl;
+            cout<<"make sure the vertical column has a value"<<endl; //error code
         }
 
     }
